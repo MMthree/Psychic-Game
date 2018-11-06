ca882467e894ed3ac405b8482588c0263f9af066
redyellowblue <42159847+MMthree@users.noreply.github.com>
2018-11-06 13:15:45 -0800

    wrote html and css

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var yourGuesses;
+var compLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "l", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+
+document.onkeyup = function(e) {
+    var userInput = e.key;
+}
+
+
+
+
+
