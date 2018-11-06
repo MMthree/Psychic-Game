09388d93b0b8aa2127243e80a61acc4385f83b83
redyellowblue <42159847+MMthree@users.noreply.github.com>
2018-11-06 15:01:56 -0800

    added some js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,33 @@
+var wins = document.getElementById("wins-text");
+var losses = document.getElementById("losses-text");
+var guessesLeft = document.getElementById("guesses-left-text");
+var lettersGuessed = document.getElementById("letters-guessed-text");
+
+
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
-var yourGuesses;
 var compLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "l", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 
 document.onkeyup = function(e) {
-    var userInput = e.key;
+
+    var userGuess = e.key;
+    var compGuess = compLetter[Math.floor(Math.random() * compLetter.length)];
+
+    if ((userGuess === compGuess)) {
+        wins++;
+    } else if ((userGuess !== compGuess)) {
+        losses--;
+        guessesLeft--;
+
+       
+    }
+        winsText.textContent = wins;
+    
 }
+
+
 
 
 
